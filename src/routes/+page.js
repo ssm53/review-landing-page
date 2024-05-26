bdf2cc602f96e8feca30f48e7f55a9cf3e1fd14c
Shaun Menezes <shaunshanil95@gmail.com>
2024-05-26 18:44:25 +0800

    large screen not good and start not showing

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,13 +1,3 @@
-// ok so here,
-
-// 1. Home page
-// says Klinik Keluarga Desa Review Page (done)
-// then shows all the 5 start as icons, non-filled (done)
-// if you click 3 star and below, it will fill the star, (done)
-// and a input box will comeout with placeholder of 'what do we need to improve on?' (done)
-// once submit form, should show a thank you modal! - use store here (done)
-
-// if click 4 star and above, it brings us to google page for them to review! (done)
-
 // To-DO
+// fix styling for large screens
 // need to find a way to store the bad recommendations and shit
--- src/routes/recommendations/+page.svelte
+++ src/routes/recommendations/+page.svelte
@@ -1,9 +1,2 @@
-<script>
-	import { recommendations } from '../../stores/store';
-</script>
 
-<!-- <div class="flex flex-col">
-	{#each $recommendations as rec}
-		<p>{rec}</p>
-	{/each}
-</div> -->
+
