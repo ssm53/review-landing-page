9416c8ab4c5f9d4393b70afcad648457218b7999
Shaun Menezes <shaunshanil95@gmail.com>
2024-05-30 13:21:24 +0800

    added datetime to reviews post manny

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,9 +1,6 @@
 // To-DO
 // fix styling for large screens
+// Also allow for to show timting and date for each review. So doc can know who and what is bad? This is all basically to understand your clinic better.
 
-//DOING
-// need to find a way to store the bad recommendations and shit
-
-// 1. create connection string thingy and set up backend - DONE
-// 1. when someone clicks 3 stars and below, a form is open... send the message to the backend api - DONE
-// 2. create backend api for that, and store it in database - DONE
+// DOING
+// Also allow for to show timting and date for each review. So doc can know who and what is bad? This is all basically to understand your clinic better.
--- src/routes/recommendations/+page.svelte
+++ src/routes/recommendations/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { goto } from '$app/navigation';
 	import { PUBLIC_BACKEND_BASE_URL } from '$env/static/public';
+	import { formatDateTime } from '../../utils/date.js';
 
 	export let data;
 </script>
@@ -12,6 +13,7 @@
 				<tr>
 					<th class="border border-gray-400 px-4 py-2">ID</th>
 					<th class="border border-gray-400 px-4 py-2">Review</th>
+					<th class="border border-gray-400 px-4 py-2">Date & Time</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -19,11 +21,12 @@
 					<tr class="hover:bg-gray-100">
 						<td class="border border-gray-400 px-4 py-2">{revDeets.id}</td>
 						<td class="border border-gray-400 px-4 py-2">{revDeets.review}</td>
+						<td class="border border-gray-400 px-4 py-2">{formatDateTime(revDeets.dateTime)}</td>
 					</tr>
 				{/each}
 			</tbody>
 		</table>
 	</div>
 {:else}
-	<p>No reviews to show</p>
+	<p>No reviews to show at the moment.</p>
 {/if}
