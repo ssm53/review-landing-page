a6b38ede6dec508079e6240102d22621748c436b
Shaun Menezes <shaunshanil95@gmail.com>
2024-05-30 16:13:11 +0800

    did desc form of ID, and removed datetime

--- src/routes/recommendations/+page.svelte
+++ src/routes/recommendations/+page.svelte
@@ -4,24 +4,27 @@
 	import { formatDateTime } from '../../utils/date.js';
 
 	export let data;
+
+	// Sort the data.allReviews array by id in descending order
+	$: sortedReviews = data.allReviews.slice().sort((a, b) => b.id - a.id);
 </script>
 
-{#if data.allReviews.length > 0}
+{#if sortedReviews.length > 0}
 	<div>
 		<table class="border-collapse w-full">
 			<thead>
 				<tr>
 					<th class="border border-gray-400 px-4 py-2">ID</th>
 					<th class="border border-gray-400 px-4 py-2">Review</th>
-					<th class="border border-gray-400 px-4 py-2">Date & Time</th>
+					<!-- <th class="border border-gray-400 px-4 py-2">Date & Time</th> -->
 				</tr>
 			</thead>
 			<tbody>
-				{#each data.allReviews as revDeets}
+				{#each sortedReviews as revDeets}
 					<tr class="hover:bg-gray-100">
 						<td class="border border-gray-400 px-4 py-2">{revDeets.id}</td>
 						<td class="border border-gray-400 px-4 py-2">{revDeets.review}</td>
-						<td class="border border-gray-400 px-4 py-2">{formatDateTime(revDeets.reviewTime)}</td>
+						<!-- <td class="border border-gray-400 px-4 py-2">{formatDateTime(revDeets.reviewTime)}</td> -->
 					</tr>
 				{/each}
 			</tbody>
